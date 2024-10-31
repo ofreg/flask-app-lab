acc733f5b9f26b010446f5f67de798c46ed46d9e
Олекандр Мацькевич <ofreg13@gmail.com>
2024-10-31 22:08:44 +0200

    scheme

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,12 @@
 from . import users_bp
 from flask import render_template, abort, request, url_for,redirect, make_response, session
 from datetime import datetime, timedelta
-from flask import flash
+from flask import flash, Flask
+
 @users_bp.route("hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
 def greetings(name):
     name = name.upper()
-    age = request.args.get("age", None, int)   
+    age = request.args.get("age", None, int)   , Flask
 
     return render_template("hi.html",name=name, age=age)
 
@@ -18,18 +19,31 @@ def admin():
 @users_bp.route("/profile")
 def get_profile():
     if "username" in session:
-        username_value= session["username"]
-        return render_template("profile.html", username=username_value)
-    flash("Invalid: Session.", "danger")
+        cookies = request.cookies  # Отримуємо куки
+        username_value = session["username"]
+        
+        # Отримуємо кольорову схему з куків, значення за замовчуванням - 'light'
+        color_scheme = request.cookies.get('color_scheme', 'light')  
+        
+        return render_template("profile.html", username=username_value, cookies=cookies, color_scheme=color_scheme)  # Передаємо куки і кольорову схему
+    flash("Сесія недійсна. Увійдіть знову.", "danger")
     return redirect(url_for("users.login"))
 
+
+VALID_USERNAME = "user"
+VALID_PASSWORD = "password123"
 @users_bp.route("/login", methods=['GET','POST'])
 def login():    
     if request.method == "POST":
         username = request.form["login"]
-        session["username"]=username
-        flash("Success: session added successfully.", "success")
-        return redirect(url_for('users.get_profile'))
+        password= request.form["password"]
+        
+        if username == VALID_USERNAME and password == VALID_PASSWORD:
+            session["username"]=username
+            flash("Успішний вхід! Ласкаво просимо!", "success")
+            return redirect(url_for('users.get_profile'))
+        else:
+            flash('Невірний логін або пароль', 'error')
     #session["age"]=30
     
     return render_template("login.html")
@@ -37,24 +51,62 @@ def login():
 @users_bp.route("/logout")
 def logout():
     session.pop('username', None)
-    session.pop('age', None)
+    #session.pop('age', None)
     return redirect(url_for('users.get_profile'))
     
-
-@users_bp.route('/set_cookie')
+@users_bp.route("/set_cookie", methods=['POST'])
 def set_cookie():
-    response = make_response('Кука встановлена')
-    response.set_cookie('username', 'student', expires=datetime.now()+timedelta(seconds=10))
-    response.set_cookie('username', 'student', max_age=timedelta(seconds=10))
+    key = request.form['key_cookie']  
+    value = request.form['value_cookie']  
+    max_age = int(request.form['time_cookie'])
+
+    
+    if not key:
+        flash("Ключ кукі не може бути пустим!", "danger")
+        return redirect(url_for('users.get_profile'))
+
+    response = make_response(redirect(url_for('users.get_profile')))  
+    response.set_cookie(key, value, max_age=max_age)  
+
+    flash("Кука успішно встановлена!", "success") 
     return response
+
 
 @users_bp.route('/get_cookie')
 def get_cookie():
     username = request.cookies.get('username')
-    return f'Користувач: {username}'
-
-@users_bp.route('/delete_cookie')
+    if username:
+        return f'Користувач: {username}'  
+    else:
+        return 'Кука не знайдена'  
+    
+@users_bp.route('/delete_cookie', methods=['POST'])
 def delete_cookie():
-    response = make_response('Кука видалена')
-    response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    key = request.form.get('value_delete_cookie')  
+    response = make_response(redirect(url_for('users.get_profile')))
+    if key and key in request.cookies:
+        response.set_cookie(key, '', expires=0)  # Вказуємо шлях
+        flash(f'Кука "{key}" видалена!', 'success')  
+    else:
+        flash(f'Кука "{key}" не існує або ключ не вказано!', 'danger')  
+
     return response
+
+
+@users_bp.route('/delete_all_cookie', methods=['POST'])
+def delete_all_cookie():
+    response = make_response(redirect(url_for('users.get_profile')))
+    for key in request.cookies.keys():
+        response.set_cookie(key, '', expires=0)  
+
+    flash('Всі куки видалені!', 'success') 
+
+    return response
+
+@users_bp.route("/set_color_scheme", methods=["POST"])
+def set_color_scheme():
+    scheme = request.form.get('color_scheme', 'light')  
+    response = make_response(redirect(url_for('users.get_profile')))
+    response.set_cookie('color_scheme', scheme)  
+    return response
+
