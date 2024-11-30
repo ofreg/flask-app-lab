45c3c30a34ea2aa6562998472f880f5134d03294
savkivdev <savkivdev@gmail.com>
2024-11-30 23:44:44 +0200

    dev-7

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app(config_name="config"):
     migrate.init_app(app,db)
     with app.app_context():
         from . import views
-
+        from .users.models import User
         from .posts import post_bp
         from .users import users_bp
         app.register_blueprint(post_bp)
--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,14 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, BooleanField,  DateField, SelectField, DateTimeLocalField
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField,  DateField, SelectField, DateTimeLocalField,SelectMultipleField
 from wtforms.validators import DataRequired, Length
 from datetime import datetime
+from .models import Tag
 CATEGORIES = [('tech', 'Tech'), ('science', 'Science'), ('lifestyle', 'Lifestyle')]
 
 class PostForm(FlaskForm):
     title= StringField('Title', validators=[DataRequired(), Length(min=2)])
     content = TextAreaField('Content', render_kw={"rows": 5,"cols":40},validators=[DataRequired()])
+    tags = SelectMultipleField(
+        "Tags",
+        coerce=int,
+        choices=[(tag.id, tag.name) for tag in Tag.query.all()]  
+    )
     is_active= BooleanField('Active Post')
     publish_date= DateTimeLocalField('Publish Date',format='%Y-%m-%dT%H:%M',default=datetime.now())
     category = SelectField('Category', choices=CATEGORIES,validators=[DataRequired()])
-    author = StringField('author')
+    author_id = SelectField("Author", coerce=int)
+
     submit = SubmitField('Add Post')
